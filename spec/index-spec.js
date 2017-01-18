39ef99f7279b7829370ca159a3a8bb643eb5ae27
Jonas Etzold <jetzold@newstore.com>
2017-01-18 13:01:08 +0100

    feat(insult): make sentence validator smarter BIN-16 (#3)

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -2,11 +2,12 @@ var index = require("../index");
 
 describe("Bingos accepting behaviour", function () {
   it("should accept full sentences", function () {
-    expect(index.containsSentence("This is a sentence.")).toBeTruthy();
-    expect(index.containsSentence("Sentence.")).toBeFalsy();
-    expect(index.containsSentence("blue cheese won't fit")).toBeFalsy();
+    expect(index.containsValidSentences("This is a sentence.")).toBe(1);
+    expect(index.containsValidSentences("Sentence.")).toBe(-1);
+    expect(index.containsValidSentences("blue cheese won't fit")).toBe(-1);
   });
   it("should accept multiple sentences", function () {
-    expect(index.containsSentence("This is a text. But it has another sentence.")).toBeTruthy();
+    expect(index.containsValidSentences("This is a text. But it has another sentence.")).toBe(1);
+    expect(index.containsValidSentences("Bingo Dredd is the law. Valerio. MacGyver. Steven Seagal. Beer. Kaka in der Kopf.")).toBe(0);
   });
 });    
